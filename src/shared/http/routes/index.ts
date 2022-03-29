63d8a5e755586fa7c61d0923dd5f5c4b2dc4e784
ewerton.marschalk <ewerton.marschalk@redeunifique.com.br>
2022-03-29 13:11:36 -0300

    token verify on routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import usuarioRouter from '@modules/usuario/routes/usuario.routes';
 import sessionRouter from '@modules/usuario/routes/session.routes';
+import isAuthenticated from '@modules/usuario/middlewares/isAuthenticated';
 
 const routes = Router();
 
-routes.use('/usuario', usuarioRouter);
+routes.use('/usuario', isAuthenticated, usuarioRouter);
 routes.use('/session', sessionRouter);
 
 routes.get('/', (request, response) => {
