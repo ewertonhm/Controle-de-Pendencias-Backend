75c0fca954c0165980d6d4d7dbfda09ebe902f4a
ewerton.marschalk <ewerton.marschalk@redeunifique.com.br>
2022-03-29 10:21:05 -0300

    routes e validação usuario

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import usuarioRouter from '@modules/usuario/routes/usuario.routes';
 
 const routes = Router();
+
+routes.use('/usuario', usuarioRouter);
 
 routes.get('/', (request, response) => {
     return response.json({ message: 'Hello World!' });
