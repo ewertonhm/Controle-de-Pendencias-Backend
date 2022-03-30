33233efe0178d55074b2fb3ebf8cd65e438d13a8
ewerton.marschalk <ewerton.marschalk@redeunifique.com.br>
2022-03-30 15:37:30 -0300

    repositorys

---
+++ src/modules/pendencia/services/CreatePendenciaService.ts
@@ -0,0 +1,36 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+interface IRequest {
+    tipo: string;
+    severidade: number;
+}
+
+class CreateTipoPendenciaService {
+    public async execute({
+        tipo,
+        severidade,
+    }: IRequest): Promise<TipoPendencia> {
+        const tipoPendenciaRepository = getCustomRepository(
+            TipoPendenciaRepository,
+        );
+        const tipoPendenciaExists = await tipoPendenciaRepository.findByTipo(
+            tipo,
+        );
+
+        if (tipoPendenciaExists) {
+            throw new AppError('Tipo já está cadastrado!');
+        }
+
+        const tipoPendencia = tipoPendenciaRepository.create({
+            tipo,
+            severidade,
+        });
+
+        await tipoPendenciaRepository.save(tipoPendencia);
+
+        return tipoPendencia;
+    }
+}
+
+export default CreateTipoPendenciaService;
