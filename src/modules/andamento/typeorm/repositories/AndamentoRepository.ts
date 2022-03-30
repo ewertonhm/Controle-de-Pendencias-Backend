00c4ffa06cebb2006e5960a891a7356a178f18ab
ewerton.marschalk <ewerton.marschalk@redeunifique.com.br>
2022-03-30 15:37:42 -0300

    repository

--- src/modules/andamento/typeorm/repositories/AndamentoRepository.ts
+++ src/modules/andamento/typeorm/repositories/AndamentoRepository.ts
@@ -1,5 +1,13 @@
+import Pendencia from '@modules/pendencia/typeorm/entities/Pendencia';
+import Usuario from '@modules/usuario/typeorm/entities/Usuario';
 import { EntityRepository, Repository } from 'typeorm';
 import Andamento from '../entities/Andamento';
+
+interface IRequest {
+    usuario: Usuario;
+    pendencia: Pendencia;
+    andamento: string;
+}
 
 @EntityRepository(Andamento)
 export class AndamentoRepository extends Repository<Andamento> {
@@ -9,5 +17,21 @@ export class AndamentoRepository extends Repository<Andamento> {
         });
 
         return andamento;
+    }
+
+    public async createAndamento({
+        usuario,
+        pendencia,
+        andamento,
+    }: IRequest): Promise<Andamento> {
+        const andamentoEntry = this.create({
+            usuario,
+            pendencia,
+            andamento,
+        });
+
+        await this.save(andamentoEntry);
+
+        return andamentoEntry;
     }
 }
--- src/modules/pendencia/typeorm/repositories/PendenciaRepository.ts
+++ src/modules/pendencia/typeorm/repositories/PendenciaRepository.ts
@@ -1,5 +1,20 @@
+import TipoPendencia from '@modules/tipo_pendencia/typeorm/entities/TipoPendencia';
+import Usuario from '@modules/usuario/typeorm/entities/Usuario';
 import { EntityRepository, Repository } from 'typeorm';
 import Pendencia from '../entities/Pendencia';
+
+interface IRequest {
+    tipoPendencia: string;
+    usuarioAbertura: string;
+    usuarioFechamento: string;
+    titulo: string;
+    descricao: string;
+    inicio: Date;
+    previsao: Date;
+    fim: Date;
+    task: Date;
+    incidente: Date;
+}
 
 @EntityRepository(Pendencia)
 export class PendenciaRepository extends Repository<Pendencia> {
@@ -11,6 +26,36 @@ export class PendenciaRepository extends Repository<Pendencia> {
                 'usuarioFechamento',
             ],
         });
+
+        return pendencia;
+    }
+
+    public async createPendencia({
+        tipoPendencia,
+        usuarioAbertura,
+        usuarioFechamento,
+        titulo,
+        descricao,
+        inicio,
+        previsao,
+        fim,
+        task,
+        incidente,
+    }: IRequest): Promise<Pendencia> {
+        const pendencia = this.create({
+            tipoPendencia,
+            usuarioAbertura,
+            usuarioFechamento,
+            titulo,
+            descricao,
+            inicio,
+            previsao,
+            fim,
+            task,
+            incidente,
+        });
+
+        await this.save(pendencia);
 
         return pendencia;
     }
