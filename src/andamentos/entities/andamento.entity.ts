9874618db1896da9461264614623727a0a92f2da
ewerton.marschalk <ewerton.marschalk@redeunifique.com.br>
2022-04-20 14:18:56 -0300

    entitys

--- src/andamentos/entities/andamento.entity.ts
+++ src/andamentos/entities/andamento.entity.ts
@@ -1,1 +1,24 @@
-export class Andamento {}
+import { Pendencia } from "src/pendencias/entities/pendencia.entity";
+import { User } from "src/users/entities/user.entity";
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Andamento {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ManyToOne(() => User, user => user.id)
+    user: User;
+
+    @ManyToOne(() => Pendencia, pendencia => pendencia.id)
+    pendencia: Pendencia;
+
+    @Column({length: 256})
+    andamento: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
--- src/pendencias/entities/pendencia.entity.ts
+++ src/pendencias/entities/pendencia.entity.ts
@@ -1,1 +1,45 @@
-export class Pendencia {}
+import { TipoPendencia } from "src/tipo_pendencias/entities/tipo_pendencia.entity";
+import { User } from "src/users/entities/user.entity";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Pendencia {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ManyToOne(() => TipoPendencia, tipoPendencia => tipoPendencia.id)
+    tipoPendencia: TipoPendencia;
+
+    @ManyToOne(() => User, userAbertura => userAbertura.id)
+    userAbertura: User;
+
+    @ManyToOne(() => User, userFechamento => userFechamento.id, {nullable: true})
+    userFechamento: User;
+
+    @Column()
+    titulo: string;
+
+    @Column({nullable: true})
+    descricao: string;
+
+    @Column()
+    inicio: Date;
+
+    @Column({nullable: true})
+    previsao: Date;
+
+    @Column({nullable: true})
+    fim: Date;
+
+    @Column({nullable: true})
+    task: string;
+
+    @Column({nullable: true})
+    incidente: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
